54eb55a765bf5534adc4a840e8b4ad5e8996f95b
Administrator <77439678@qq.com>
2019-06-18 06:30:51 +0800

    Initial commit

---
+++ vue-demo1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+     <img class="logo" src="./assets/logo.png" alt="logo"/>
+    <!--使用组件标签-->
+    <HelloWorld></HelloWorld>
+
+  </div>
+</template>
+
+
+<script>
+  //1引入组件
+  import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    //2映射组件标签
+    components:{
+      HelloWorld
+    }
+
+  }
+</script>
+
+<style>
+ .logo{
+   width: 200px;
+   height: 200px;
+ }
+
+</style>
---
+++ vue-demo1/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p class="msg">{{msg}}</p>
+    </div>
+</template>
+
+
+<script>
+    export default {//配置选项和Vue一致
+       data(){//必须写函数
+         return{
+           msg:"Hello VUE Component"
+         }
+       }
+    }
+</script>
+
+<style>
+  .msg{
+    color: red;
+    font-size: 30px;
+  }
+
+</style>
